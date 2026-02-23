c08510c27f2cd7833be3412807c14083593d69e4
Denis Muravec <denysmuravets@gmail.com>
2026-02-23 19:16:04 +0100

    *

--- js/change-urls-script.js
+++
@@ -1,9 +0,0 @@
-var elements = document.getElementsByClassName("AddGitHubUrlToHref");
-        elements.forEach(element => {
-            element.href = "/remont.github.io" + element.href;
-        });
-
-elements = document.getElementsByClassName("AddGitHubUrlToSrc");
-        elements.forEach(element => {
-            element.src = "/remont.github.io" + element.src;
-        });
