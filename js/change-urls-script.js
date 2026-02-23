09e675d73c0fe304642c37f77fbb4071c83432f7
Denis Muravec <denysmuravets@gmail.com>
2026-02-23 18:31:19 +0100

    виніс скрипт заміни адреси в окремий файл

---
+++ js/change-urls-script.js
@@ -0,0 +1,9 @@
+var elements = document.getElementsByClassName("AddGitHubUrlToHref");
+        elements.forEach(element => {
+            element.href += "/remont.github.io";
+        });
+
+elements = document.getElementsByClassName("AddGitHubUrlToSrc");
+        elements.forEach(element => {
+            element.src += "/remont.github.io";
+        });
